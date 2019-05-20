0e2a5e4b6761b995f9244ce96d7763f7aa6d50d7
luwt <luwentao@alwaysnb.com>
2019-05-20 09:54:34 +0800

    交互完善

--- table_info.py
+++ table_info.py
@@ -2,17 +2,26 @@
 from db_opt import getcursor
 from docx_demo import index_type
 import docx
+import os
+import time
 _author_ = 'luwt'
 _date_ = '2018/12/17 13:42'
 
 
 class TableInfo:
 
-    def __init__(self):
-        self.doc = docx.Document()
+    def __init__(self, table_schema, path):
+        default_docx = 'default.docx' if os.path.exists(
+            os.path.join(os.path.abspath('.'), 'default.docx')) else None
+        self.doc = docx.Document(docx=default_docx)
+        self.cursor = TableInfo.get_cur()
+        self.schema = table_schema
+        self.path = path
+
+    @staticmethod
+    def get_cur():
         get_cursor = getcursor.GetCursor()
-        conn = get_cursor.get_native_conn()
-        self.cursor = conn.cursor()
+        return get_cursor.get_native_conn().cursor()
 
     def get_data(self, sql):
         self.cursor.execute(sql)
@@ -52,7 +61,7 @@ class TableInfo:
     def main(self):
         # 查表名加表注释
         table_names_sql = 'SELECT table_name, table_comment from information_schema.`TABLES` ' \
-                          'where table_schema="test" and table_comment!="";'
+                          'where table_schema = "{}" and table_comment != "";'.format(self.schema)
         # 查出所有表名
         table_names = self.get_data(table_names_sql)
         for table_name in table_names:
@@ -63,9 +72,32 @@ class TableInfo:
             data = self.get_data(table_infos_sql)
             self.generate_table_title(table_comment, table_name)
             self.generate_table(data)
-        self.doc.save('D:\\test.docx')
+        self.doc.save(self.path)
+        print('执行完毕，输出为{}，三秒后退出！'.format(path))
+        time.sleep(3)
+
+    @staticmethod
+    def get_schema(schema):
+        cur = getcursor.GetCursor().get_native_schema_conn().cursor()
+        cur.execute('SELECT SCHEMA_NAME FROM `SCHEMATA`;')
+        data = cur.fetchall()
+        data = list(map(lambda x: str(x[0]), data))
+        print('正确的数据库名为：{}'.format(data))
+        if schema in data:
+            return True
+        return False
 
 
 if __name__ == '__main__':
-    table_info = TableInfo()
+    while True:
+        name = input('请输入数据库名称：')
+        flag = TableInfo.get_schema(name)
+        if flag:
+            break
+        continue_ = input('请输入正确的数据库名称，继续？y/n')
+        if continue_ is 'n':
+            exit()
+    path = 'D:\\数据词典demo.docx'
+    table_info = TableInfo('test', path)
     table_info.main()
+
