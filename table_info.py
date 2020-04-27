75f3b39a4f31907956dc26df745884518ec3873f
逯文涛 <847466569@qq.com>
2020-04-27 12:12:53 +0800

    修改引包声明，不声明系统路径也能检索到，暂时去掉了

--- table_info.py
+++ table_info.py
@@ -3,14 +3,9 @@ from get_cursor import Cursor
 import index_type
 import docx
 import os
-import sys
 import time
 _author_ = 'luwt'
 _date_ = '2018/12/17 13:42'
-
-
-current = os.getcwd()
-sys.path.append(current)
 
 
 # 从系统库中查出所有数据库的名称，校验输入数据库的正确性
